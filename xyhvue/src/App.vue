e2506ac0acda39548bc05648f25455c2d548dffc
xyh <xyhnnx@163.com>
2017-10-19 09:53:28 +0800

    手机键盘弹起底部导航移位问题

--- xyhvue/src/App.vue
+++ xyhvue/src/App.vue
@@ -5,15 +5,25 @@
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  window.onload = () => {
+    // 获取窗口高度
+    let winHeight
+    if (window.innerHeight) {
+      winHeight = window.innerHeight
+    } else if ((document.body) && (document.body.clientHeight)) {
+      winHeight = document.body.clientHeight
+    }
+    document.body.style.minHeight = winHeight + 'px'
+  }
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 </style>
